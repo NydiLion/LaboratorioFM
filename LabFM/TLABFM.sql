b0996c4c79d66b1ab255711e36f53e817df32f60
NydiLion <snaker2010@hotmail.es>
2020-04-17 20:59:23 -0600

    Finalizacion de Ventana devolucion Vencimiento con su base de datos Actualizada :v

--- LabFM/TLABFM.sql
+++ LabFM/TLABFM.sql
@@ -1,55 +1,66 @@
 CREATE DATABASE labFm;
 USE labFm;
+
 CREATE TABLE CLIENTES
 (
-ID int(20) auto_increment NOT NULL,
-CodigoCliente VARCHAR (10) PRIMARY KEY,
-NombreCliente VARCHAR (50) NOT NULL,
-DpiCliente VARCHAR (50) NOT NULL,
-DireccionCliente VARCHAR (50) NOT NULL,
-TelefonoCliente VARCHAR (10) NOT NULL,
-CorreoCliente VARCHAR (30) NOT NULL,
-EstatusCliente VARCHAR (10) NOT NULL
+ID int(20) auto_increment ,
+CodigoCliente VARCHAR (10),
+NombreCliente VARCHAR (50),
+DpiCliente VARCHAR (50),
+DireccionCliente VARCHAR (50),
+TelefonoCliente VARCHAR (10),
+CorreoCliente VARCHAR (30),
+EstatusCliente VARCHAR (10), 
+PRIMARY KEY (CodigoCliente)
 ) ENGINE=INNODB DEFAULT CHARSET=latin1;
 
 CREATE TABLE PELICULAS
 (
-ID int(20) auto_increment NOT NULL,
-CodigoPelicula  VARCHAR (10) PRIMARY KEY,
-NombrePelicula VARCHAR (50) NOT NULL,
-AutorPelicula VARCHAR (50) NOT NULL,
-TipoPelicula VARCHAR (10) NOT NULL,
-ExistenciaPelicula VARCHAR (20) NOT NULL,
-EstatusPelicula VARCHAR (10) NOT NULL
+ID int(20) auto_increment,
+CodigoPelicula  VARCHAR (10),
+NombrePelicula VARCHAR (50),
+AutorPelicula VARCHAR (50),
+TipoPelicula VARCHAR (10),
+ExistenciaPelicula VARCHAR (20),
+EstatusPelicula VARCHAR (10),
+PRIMARY KEY (CodigoPelicula)
 ) ENGINE=INNODB DEFAULT CHARSET=latin1;
 
 CREATE TABLE VIDEOJUEGOS
 (
-ID int(20) auto_increment NOT NULL,
-CodigoVideoJ  VARCHAR (10) PRIMARY KEY,
-NombreVideoJ VARCHAR (50) NOT NULL,
-ConsolaVideoJ VARCHAR (50) NOT NULL,
-TipoVideoJ VARCHAR (10) NOT NULL,
-ExistenciaVideoJ VARCHAR (20) NOT NULL,
-EstatusVideoJ VARCHAR (10) NOT NULL
+ID int(20) auto_increment,
+CodigoVideoJ  VARCHAR (10),
+NombreVideoJ VARCHAR (50),
+ConsolaVideoJ VARCHAR (50),
+TipoVideoJ VARCHAR (10),
+ExistenciaVideoJ VARCHAR (20),
+EstatusVideoJ VARCHAR (10),
+PRIMARY KEY (CodigoVideoJ)
 ) ENGINE=INNODB DEFAULT CHARSET=latin1;
 
-CREATE TABLE RENTAVENCIDA
+CREATE TABLE FVENCIDA
 (
+ID int(20) auto_increment,
+CodigoDevVencida VARCHAR (10), 
 CodigoCliente VARCHAR (10),
 CodigoPelicula VARCHAR (10),
-CodigoVideoJ VARCHAR (10), 
-FechaRenta DATE,
-FechaDevolucion DATE,
-FechaVencida DATE,
-ValorVencido FLOAT(10,2),
-PagoCores FLOAT(10,2),
-PRIMARY KEY (CodigoCliente, CodigoPelicula, CodigoVideoJ),
-FOREIGN KEY (CodigoCliente) REFERENCES CLIENTES (CodigoCliente),
-FOREIGN KEY (CodigoPelicula) REFERENCES PELICULAS (CodigoPelicula),
-FOREIGN KEY (CodigoVideoJ) REFERENCES VIDEOJUEGOS (CodigoVideoJ)
+CodigoVideoJ VARCHAR (10),
+FechaRenta VARCHAR (10),
+FechaDevolucion VARCHAR (10),
+FechaVencida VARCHAR (10),
+PagoImporte VARCHAR (10),
+PRIMARY KEY (CodigoDevVencida),
+KEY (ID)
 ) ENGINE=INNODB DEFAULT CHARSET=latin1;
+
+USE labFm;
+Select * From CLIENTES;
 
 USE labFm;
 Select * From PELICULAS;
 
+USE labFm;
+Select * From VIDEOJUEGOS;
+
+USE labFm;
+Select * From FVENCIDA;
--- LabFM/src/FilmMagic/RegistroDevVencidas.java
+++ LabFM/src/FilmMagic/RegistroDevVencidas.java
@@ -4,7 +4,11 @@
  * and open the template in the editor.
  */
 package FilmMagic;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
 /**
  *
  * @author snake
@@ -27,26 +31,327 @@ public class RegistroDevVencidas extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        txtCodigoDevV = new javax.swing.JTextField();
+        txtCodigCliente = new javax.swing.JTextField();
+        txtCodigoPeli = new javax.swing.JTextField();
+        textCodVideoJ = new javax.swing.JTextField();
+        txtFRenta = new javax.swing.JTextField();
+        textFDev = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
+        txtFvencida = new javax.swing.JTextField();
+        jLabel8 = new javax.swing.JLabel();
+        txtPimporte = new javax.swing.JTextField();
+        registro = new javax.swing.JButton();
+        eliminar = new javax.swing.JButton();
+        modificar = new javax.swing.JButton();
+        jLabel9 = new javax.swing.JLabel();
+        txt_buscar = new javax.swing.JTextField();
+        buscar = new javax.swing.JButton();
+        label_status = new javax.swing.JLabel();
+
         setClosable(true);
         setIconifiable(true);
         setMaximizable(true);
         setVisible(true);
 
+        jLabel1.setText("Codigo Devolucion Vencida");
+
+        jLabel2.setText("Codigo Cliente");
+
+        jLabel3.setText("Codigo Pelicula");
+
+        jLabel4.setText("Codigo VideoJuego");
+
+        jLabel5.setText("Fecha Renta");
+
+        jLabel6.setText("Fecha Devolucion");
+
+        jLabel7.setText("Fecha Vencida");
+
+        jLabel8.setText("Pago Importe");
+
+        registro.setText("REGISTRO");
+        registro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                registroActionPerformed(evt);
+            }
+        });
+
+        eliminar.setText("ELIMINAR");
+        eliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                eliminarActionPerformed(evt);
+            }
+        });
+
+        modificar.setText("MODIFICAR");
+        modificar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                modificarActionPerformed(evt);
+            }
+        });
+
+        jLabel9.setText("Codigo Vencida Devolucion");
+
+        buscar.setText("BUSCAR");
+        buscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buscarActionPerformed(evt);
+            }
+        });
+
+        label_status.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 635, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(jLabel4)
+                                .addGroup(layout.createSequentialGroup()
+                                    .addComponent(jLabel1)
+                                    .addGap(82, 82, 82)
+                                    .addComponent(jLabel2))
+                                .addGroup(layout.createSequentialGroup()
+                                    .addComponent(txtCodigoDevV, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGap(110, 110, 110)
+                                    .addComponent(txtCodigCliente, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(textCodVideoJ)
+                                        .addComponent(jLabel7)
+                                        .addComponent(txtFvencida, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE))
+                                    .addComponent(registro))
+                                .addGap(110, 110, 110)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(eliminar)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(txtFRenta)
+                                        .addComponent(txtPimporte)
+                                        .addGroup(layout.createSequentialGroup()
+                                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                                .addComponent(jLabel8)
+                                                .addComponent(jLabel5))
+                                            .addGap(0, 0, Short.MAX_VALUE))))))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(174, 174, 174)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(jLabel3)
+                                    .addComponent(txtCodigoPeli)
+                                    .addComponent(textFDev, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
+                                    .addComponent(jLabel6)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(63, 63, 63)
+                                .addComponent(modificar))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(67, 67, 67)
+                        .addComponent(label_status, javax.swing.GroupLayout.PREFERRED_SIZE, 353, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel9)
+                        .addGap(71, 71, 71)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(buscar)
+                            .addComponent(txt_buscar, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(42, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 370, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtCodigoDevV, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtCodigCliente, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtCodigoPeli, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(43, 43, 43)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel6))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(textCodVideoJ, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtFRenta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(textFDev, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(29, 29, 29)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel7)
+                    .addComponent(jLabel8))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtFvencida, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtPimporte, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(registro)
+                    .addComponent(eliminar)
+                    .addComponent(modificar))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(label_status, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel9)
+                    .addComponent(txt_buscar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(buscar)
+                .addContainerGap(51, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void registroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_registroActionPerformed
+        try{
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/labfm", "root", "");
+            PreparedStatement pst = cn.prepareStatement("insert into fvencida values(?,?,?,?,?,?,?,?,?)");
+            pst.setString(1, "0");
+            pst.setString(2, txtCodigoDevV.getText().trim());
+            pst.setString(3, txtCodigCliente.getText().trim());
+            pst.setString(4, txtCodigoPeli.getText().trim());
+            pst.setString(5, textCodVideoJ.getText().trim());
+            pst.setString(6, txtFRenta.getText().trim());
+            pst.setString(7, textFDev.getText().trim());
+            pst.setString(8, txtFvencida.getText().trim());
+            pst.setString(9, txtPimporte.getText().trim());
+            pst.executeUpdate();
+            txtCodigoDevV.setText("");
+            txtCodigCliente.setText("");
+            txtCodigoPeli.setText("");
+            textCodVideoJ.setText("");
+            txtFRenta.setText("");
+            textFDev.setText("");
+            txtFvencida.setText("");
+            txtPimporte.setText("");
+            label_status.setText("Registro exitoso.");
+            {
+                JOptionPane.showMessageDialog(null, "Registro Exitosamente Vencimiento.");
+            }
+        }catch (Exception e){
+
+        }
+    }//GEN-LAST:event_registroActionPerformed
+
+    private void eliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_eliminarActionPerformed
+        //Codigo que permite borrar registros en la base de datos
+        try {
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/labfm", "root", "");
+            PreparedStatement pst = cn.prepareStatement("delete from fvencida where CodigoVideoJ = ?");
+
+            pst.setString(1, txt_buscar.getText().trim());
+            pst.executeUpdate();
+
+            txtCodigoDevV.setText("");
+            txtCodigCliente.setText("");
+            txtCodigoPeli.setText("");
+            textCodVideoJ.setText("");
+            txtFRenta.setText("");
+            textFDev.setText("");
+            txtFvencida.setText("");
+            txtPimporte.setText("");
+            label_status.setText("Registro eliminado Exitosamente.");
+            {
+                JOptionPane.showMessageDialog(null, "Registro de Vencimiento Eliminado.");
+            }
+        } catch (Exception e) {
+        }
+    }//GEN-LAST:event_eliminarActionPerformed
+
+    private void modificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_modificarActionPerformed
+        //Codigo que permite actualizar registros en la base de datos
+        try {
+            String ID = txt_buscar.getText().trim();
+
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/labfm", "root", "");
+            PreparedStatement pst = cn.prepareStatement("update fvencida set CodigoDevVencida = ?, CodigoCliente = ?, CodigoPelicula = ?, CodigoVideoJ = ?, FechaRenta = ?, FechaDevolucion = ?, FechaVencida = ?, PagoImporte = ? where ID = " + ID);
+
+            pst.setString(1, "0");
+            pst.setString(1, txtCodigoDevV.getText().trim());
+            pst.setString(2, txtCodigCliente.getText().trim());
+            pst.setString(3, txtCodigoPeli.getText().trim());
+            pst.setString(4, textCodVideoJ.getText().trim());
+            pst.setString(5, txtFRenta.getText().trim());
+            pst.setString(6, textFDev.getText().trim());
+            pst.setString(7, txtFvencida.getText().trim());
+            pst.setString(8, txtPimporte.getText().trim());
+            pst.executeUpdate();
+
+            label_status.setText("Modificación exitosa.");
+            {
+                JOptionPane.showMessageDialog(null, "Registro Actualizado Exitosamente.");
+            }
+        } catch (Exception e) {
+        }
+    }//GEN-LAST:event_modificarActionPerformed
+
+    private void buscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buscarActionPerformed
+        //Codigo que permite consultar registros en la base de datos
+        try{
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/labfm", "root", "");
+            PreparedStatement pst = cn.prepareStatement("select * from fvencida where ID = ?");
+            pst.setString(1, txt_buscar.getText().trim());
+
+            ResultSet rs = pst.executeQuery();
+
+            if(rs.next()){
+                txtCodigoDevV.setText(rs.getString("CodigoDevVencida"));
+                txtCodigCliente.setText(rs.getString("CodigoCliente"));
+                txtCodigoPeli.setText(rs.getString("CodigoPelicula"));
+                textCodVideoJ.setText(rs.getString("CodigoVideoJ"));
+                txtFRenta.setText(rs.getString("FechaRenta"));
+                textFDev.setText(rs.getString("FechaDevolucion"));
+                txtFvencida.setText(rs.getString("FechaVencida"));
+                txtPimporte.setText(rs.getString("PagoImporte"));
+            } else {
+                JOptionPane.showMessageDialog(null, "Registro de Vencimiento no Registrado.");
+            }
+
+        }catch (Exception e){
+
+        }
+    }//GEN-LAST:event_buscarActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton buscar;
+    private javax.swing.JButton eliminar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JLabel label_status;
+    private javax.swing.JButton modificar;
+    private javax.swing.JButton registro;
+    private javax.swing.JTextField textCodVideoJ;
+    private javax.swing.JTextField textFDev;
+    private javax.swing.JTextField txtCodigCliente;
+    private javax.swing.JTextField txtCodigoDevV;
+    private javax.swing.JTextField txtCodigoPeli;
+    private javax.swing.JTextField txtFRenta;
+    private javax.swing.JTextField txtFvencida;
+    private javax.swing.JTextField txtPimporte;
+    private javax.swing.JTextField txt_buscar;
     // End of variables declaration//GEN-END:variables
 }
